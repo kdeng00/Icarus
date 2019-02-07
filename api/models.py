b54bcf336833753cb7c589a7ab620859e073ce1d
amazing-username <kundeng94@gmail.com>
2019-02-06 19:11:39 -0500

    Created the Song model #2

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Models):
+    title = models.CharField(max_length=70)
+    albumn = models.CharField(max_length=70)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=70)
+    track_number = models.IntegerField()
