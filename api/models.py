3cf708485fe18adf7758dd63e5b2309d716e8f0e
amazing-username <kundeng94@gmail.com>
2019-02-08 18:25:50 +0000

    Added binary field to song model #4

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Song(models.Models):
+class Song(models.Model):
     title = models.CharField(max_length=70)
     albumn = models.CharField(max_length=70)
     year = models.IntegerField()
     genre = models.CharField(max_length=70)
     track_number = models.IntegerField()
+    file = models.BinaryField(editable=True)
