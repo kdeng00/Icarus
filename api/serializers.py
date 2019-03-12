0b6d5b05f89c325d4f424773ba1d043e39b09ad9
amazing-username <kdeng02@aurora.edu>
2019-03-11 22:31:09 -0400

    Blank slate #10

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Song
-
-class SongSerialzer(serializers.ModelSerializer):
-    class Meta:
-        model = Song
-        fields = ('id', 'title', 'albumn', 'year', 'genre', 'track_number', 'file')
