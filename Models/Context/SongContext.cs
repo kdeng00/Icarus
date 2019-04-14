0c47da3665449dfe49cb81f97f1f2cee9d8f62c0
amazing-username <kundeng94@gmail.com>
2019-04-14 18:12:10 -0400

    Provided solution for Migrations. Will test on the server #13

---
+++ Models/Context/SongContext.cs
@@ -0,0 +1,28 @@
+using System;    
+using System.Collections.Generic;    
+
+using Microsoft.EntityFrameworkCore;
+using MySql.Data;
+using MySql.Data.EntityFrameworkCore.Extensions;
+using MySql.Data.MySqlClient;    
+
+using Icarus.Models;
+        
+namespace Icarus.Models.Context    
+{    
+    public class SongContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>();
+        }
+
+		    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    		{
+      			optionsBuilder.UseMySQL("server=;database=;user=;password=");
+    		}
+
+    }
+}  
