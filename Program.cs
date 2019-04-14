ed6ae7e3ddd67001915d043a7830b580299d764e
amazing-username <kundeng94@gmail.com>
2019-04-14 16:11:48 +0000

    Removed a lingering solution file that was not needed. Within Program.cs I changed the port to 5002

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Icarus
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+		.UseUrls("http://localhost:5002");
     }
 }
