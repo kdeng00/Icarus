d0f6f400c0470ed5eff0f781fec25d7359fc5bf9
amazing-username <kundeng94@gmail.com>
2019-04-15 20:29:05 -0400

    Added User model #23

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+using System;
+
+using Newtonsoft.Json;
+
+namespace Icarus.Models
+{
+	public class User
+	{
+		[JsonProperty("id")]
+		public int Id { get; set; }
+		[JsonProperty("username")]
+		public string Username { get; set; }
+		[JsonProperty("password")]
+		public string Password { get; set; }
+		[JsonProperty("email")]
+		public string Email { get; set; }
+		[JsonProperty("phone_number")]
+		public string PhoneNumber { get; set; }
+		[JsonProperty("first_name")]
+		public string Firstname { get; set; }
+		[JsonProperty("last_name")]
+		public string Lastname { get; set; }
+		[JsonProperty("date_created")]
+		public DateTime DateCreated { get; set; }
+		[JsonProperty("last_login")]
+		public DateTime LastLogin { get; set; }
+	}
+}
