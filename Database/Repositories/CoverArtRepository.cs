2b59bfac327e65cdc1cdce2405c822ebe81f2f0d
amazing-username <kundeng94@gmail.com>
2019-07-08 18:42:44 -0400

    Updates to the Cover art repository #51

--- Database/Repositories/CoverArtRepository.cs
+++ Database/Repositories/CoverArtRepository.cs
@@ -10,82 +10,125 @@ namespace Icarus.Database.Repositories
     public class CoverArtRepository : BaseRepository
     {
         #region Constructors
-	public CoverArtRepository(string connectionString)
-	{
+        public CoverArtRepository(string connectionString)
+        {
             _connectionString = connectionString;
-	}
-	#endregion
+        }
+        #endregion
 
 
-	#region Methods
-	public CoverArt GetCoverArt(Song song)
-	{
-	    // TODO: Implement sql record retrieval
-	    return null;
-	}
+        #region Methods
+        public CoverArt GetCoverArt(CoverArt coverArt)
+        {
+            // TODO: Implement sql record retrieval
+            //
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+            return null;
+        }
+        public CoverArt GetCoverArt(Song song)
+        {
+            // TODO: Implement sql record retrieval
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+            return null;
+        }
 
-	public void DeleteCoverArt(CoverArt coverArt)
-	{
-	    try
-	    {
-	        // TODO: Implement sql record deletion
-	    }
-	    catch (Exception ex)
-	    {
-	        var msg = ex.Message;
-		_logger.Error(msg, "An error occurred");
-	    }
-	}
+        public void SaveCoverArt(CoverArt coverArt)
+        {
+            try
+            {
+                using (var conn = GetConnection())
+                {
+                    conn.Open();
 
-	private List<CoverArt> ParseData(MySqlDataReader reader)
-	{
-	    if (reader.HasRows)
-	    {
-	        var coverArtList = new List<CoverArt>();
-	        while (reader.Read())
-	            coverArtList.Add(new CoverArt
-		    {
-		        CoverArtId = Convert.ToInt32(reader["CoverArtId"]),
-		        SongTitle = reader["SongTitle"].ToString(),
-		        ImagePath = reader["ImagePath"].ToString()
-		    });
+                    var query = "INSERT INTO CoverArt(SongTitle, ImagePath) VALUES(@SongTitle, " +
+                        "@ImagePath)";
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@SongTitle", coverArt.SongTitleA);
+                        cmd.Parameters.AddWithValue("@ImagePath", coverArt.ImagePath);
 
-		return coverArtList;
-	    }
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+        }
+        public void DeleteCoverArt(CoverArt coverArt)
+        {
+            try
+            {
+                // TODO: Implement sql record deletion
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                _logger.Error(msg, "An error occurred");
+            }
+        }
 
-	    return null;
-	}
-	
-	private CoverArt ParseSingleData(MySqlDataReader reader)
-	{
-	    if (reader.HasRows)
-	    {
-	        reader.Read();
+        private List<CoverArt> ParseData(MySqlDataReader reader)
+        {
+            if (reader.HasRows)
+            {
+                var coverArtList = new List<CoverArt>();
+                while (reader.Read())
+                    coverArtList.Add(new CoverArt
+                    {
+                        CoverArtId = Convert.ToInt32(reader["CoverArtId"]),
+                        SongTitle = reader["SongTitle"].ToString(),
+                        ImagePath = reader["ImagePath"].ToString()
+                    });
 
-		return new CoverArt
-		{
-		    CoverArtId = Convert.ToInt32(reader["CoverArtId"]),
-		    SongTitle = reader["SongTitle"].ToString(),
-		    ImagePath = reader["ImagePath}"].ToString()
-		};
-	    }
+                return coverArtList;
+            }
 
-	    return null;
-	}
+            return null;
+        }
+    
+        private CoverArt ParseSingleData(MySqlDataReader reader)
+        {
+            if (reader.HasRows)
+            {
+                reader.Read();
 
-	private bool? AnyCoverArt()
-	{
-	    using (var conn = GetConnection())
-	    {
-	        conn.Open();
+                return new CoverArt
+                {
+                    CoverArtId = Convert.ToInt32(reader["CoverArtId"]),
+                    SongTitle = reader["SongTitle"].ToString(),
+                    ImagePath = reader["ImagePath}"].ToString()
+                };
+            }
 
-		var query = "SELECT * FROM CoverArt";
+            return null;
+        }
 
-		using (var cmd = new MySqlCommand(query, conn))
-		    using (var reader = cmd.ExecuteReader())
-		        return reader.HasRows;
-	    }
-	}
-	#endregion
+        private bool? AnyCoverArt()
+        {
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+
+                var query = "SELECT * FROM CoverArt";
+                using (var cmd = new MySqlCommand(query, conn))
+                    using (var reader = cmd.ExecuteReader())
+                        return reader.HasRows;
+            }
+        }
+        #endregion
     }
 }
