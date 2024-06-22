96780752eeb7199d54230af14b8d63e8afd4a9bf
kdeng00 <kundeng94@gmail.com>
2024-06-22 13:15:36 -0400

    Added script shell to convert wav files to flac

---
+++ Scripts/convert_wav_to_flac.zsh
@@ -0,0 +1,28 @@
+#!/usr/bin/zsh
+# 
+# Working directory
+# Output directory
+# Folder name creation
+# Sub-folder name creation
+
+WORK_DIR=$1
+OUTPUT_DIR=$2
+FOLDER=$3
+SUB_FOLDER=$4
+
+
+# Validation
+# Check if the Folder and Sub folder has been created
+# Check of WORK_DIR is an actual directory
+# Check if OUTPUT_DIR exists
+# Check if WORK_DIR contains .wav files
+
+
+# Conversion
+# Convert wav files to flac files
+# Create folder and sub folder if it does not exist
+# Move the converted flac files to the sub folder
+
+# Final
+# Check if OUTPUT_DIR has the folder structure. Delete if so
+# Move the folder and sub folder to OUTPUT_DIR
