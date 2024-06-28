99e87a4c3a0ca272b87a0312ff33b99e314c2c67
kdeng00 <kundeng94@gmail.com>
2024-06-27 20:45:05 -0400

    #93: Minor change to script to convert wav files to flac

--- Scripts/convert_wav_to_flac.zsh
+++ Scripts/convert_wav_to_flac.zsh
@@ -5,10 +5,26 @@
 # Folder name creation
 # Sub-folder name creation
 
+echo "tt"
+
+if [[ $# -eq 0 ]]; then
+
+    my_text="No arguments provided. Provide four arguments:\n"
+    my_text+="script.zsh \"working_dir\" \"output_dir\" \"folder_name\" \"sub_folder_name\""
+
+    echo "$my_text"
+    exit
+fi
+
 WORK_DIR=$1
 OUTPUT_DIR=$2
 FOLDER=$3
 SUB_FOLDER=$4
+
+echo "Working directory: $WORK_DIR"
+echo "Output directory: $OUTPUT_DIR"
+echo "Folder name: $FOLDER"
+echo "Sub folder name: $SUB_FOLDER"
 
 
 # Validation
@@ -26,3 +42,4 @@ SUB_FOLDER=$4
 # Final
 # Check if OUTPUT_DIR has the folder structure. Delete if so
 # Move the folder and sub folder to OUTPUT_DIR
+
