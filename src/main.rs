a57898cf5204b0f9effe5c0e6147ebafbea535ff
KD <kundeng94@gmail.com>
2025-03-29 09:54:56 -0400

    Some changes

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,19 @@ async fn main() {
         // .route("/users", post(create_user));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(get_full()).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+fn get_full() -> String {
+    get_address() + ":" + &get_port()
+}
+fn get_address() -> String {
+    String::from("0.0.0.0")
+}
+
+fn get_port() -> String {
+    String::from("3000")
 }
 
 // basic handler that responds with a static string
