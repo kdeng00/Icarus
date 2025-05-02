ecc200517840055f9409e7dc85e9878bec61bee0
kdeng00 <kundeng00@pm.me>
2025-05-01 21:17:06 -0400

    Migration changes

--- migrations/20250420185217_init_migration.sql
+++ migrations/20250420185217_init_migration.sql
@@ -17,5 +17,12 @@ CREATE TABLE IF NOT EXISTS "metadataQueue" (
     song_queue_id UUID NOT NULL
 );
 
+-- Table to store queued coverart
+CREATE TABLE IF NOT EXISTS "coverartQueue" {
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    data BYTEA NOT NULL,
+    song_queue_id UUID NOT NULL
+};
+
 -- Create an index for better query performance
 CREATE INDEX metadata_queue_data_metadata ON "metadataQueue" USING gin (metadata);
