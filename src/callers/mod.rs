4222ebc52d9fbe33b7003a4431ae284cb7542e66
kdeng00 <kundeng00@pm.me>
2025-05-22 15:53:46 -0400

    Fixed endoint uri

--- src/callers/mod.rs
+++ src/callers/mod.rs
@@ -5,7 +5,7 @@ pub mod song;
 pub mod endpoints {
     pub const QUEUESONG: &str = "/api/v2/song/queue";
     pub const QUEUESONGDATA: &str = "/api/v2/song/queue/{id}";
-    pub const QUEUESONGUPDATE: &str = "/api/v2/song/queue/:id";
+    pub const QUEUESONGUPDATE: &str = "/api/v2/song/queue/{id}";
     pub const NEXTQUEUESONG: &str = "/api/v2/song/queue/next";
     pub const QUEUEMETADATA: &str = "/api/v2/song/metadata/queue";
     pub const QUEUECOVERART: &str = "/api/v2/coverart/queue";
