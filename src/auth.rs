c9f6fab746106b4f76898d765061471e7f3127ef
kdeng00 <kundeng00@pm.me>
2025-07-30 20:03:24 -0400

    Warning fix

--- src/auth.rs
+++ src/auth.rs
@@ -69,8 +69,8 @@ pub async fn auth<B>(
                 .get(axum::http::header::AUTHORIZATION)
                 .and_then(|auth_header| auth_header.to_str().ok())
                 .and_then(|auth_value| {
-                    if auth_value.starts_with("Bearer ") {
-                        Some(auth_value[7..].to_owned())
+                    if let Some(stripped) = auth_value.strip_prefix("Bearer ") {
+                        Some(String::from(stripped))
                     } else {
                         None
                     }
