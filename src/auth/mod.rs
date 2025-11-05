f33acdc2517a8cf3f961020a09a36817d47bf24a
kdeng00 <kundeng00@pm.me>
2025-11-05 09:50:32 -0500

    tsk-230: Cleanup

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -6,48 +6,8 @@ use axum::{
 };
 use axum_extra::extract::cookie::CookieJar;
 use jsonwebtoken::{DecodingKey, Validation, decode};
-use serde::Serialize;
 
-// TODO: Put this in icarus_models
-/*
-fn deserialize_i64_from_f64<'de, D>(deserializer: D) -> Result<i64, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    let val = f64::deserialize(deserializer)?;
-    // Handle NaN and infinity cases
-    if val.is_nan() || val.is_infinite() {
-        return Err(serde::de::Error::custom("invalid float value"));
-    }
-    // Round to nearest integer and convert
-    let rounded = val.round();
-    // Check if the rounded value can fit in i64
-    if rounded < (i64::MIN as f64) || rounded > (i64::MAX as f64) {
-        return Err(serde::de::Error::custom("float out of i64 range"));
-    }
-    Ok(rounded as i64)
-}
-*/
-
-// TODO: Put this in icarus_models
-/*
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct UserClaims {
-    pub iss: String,
-    pub aud: String, // Audience
-    pub sub: String, // Subject (user ID)
-    #[serde(deserialize_with = "deserialize_i64_from_f64")]
-    pub exp: i64, // Expiration time (UTC timestamp)
-    #[serde(deserialize_with = "deserialize_i64_from_f64")]
-    pub iat: i64, // Issued at (UTC timestamp)
-    // pub azp: String,
-    // pub gty: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub roles: Option<Vec<String>>, // Optional roles
-}
-*/
-
-#[derive(Debug, Serialize)]
+#[derive(Debug, serde::Serialize)]
 pub struct ErrorResponse {
     pub status: &'static str,
     pub message: String,
